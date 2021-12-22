4605868ba1b26960a6903fc8d77f8787ea664004
Connor Barr <crnbarr@gmail.com>
2021-12-22 14:33:25 +0000

    Added filesize check to Github Actions

--- .github/file-size.sh
+++ .github/file-size.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-for FILENAME in ./artifacts/*
 
-    do
-    FILESIZE=$(wc -c "$FILENAME" | awk '{print $1}')
-        FILESIZE=$(stat -printf='%s' "$FILENAME")
-        echo "Size of $FILENAME = $FILESIZE bytes."
-
-    done
+find ./artifacts -maxdepth 1 -size +575k | while read file; do
+  echo "$file is too large: $(wc -c "$file" | awk '{print $1}') bytes. Maximum: 575000 bytes."
+  exit 1
+done
 
 exit 0
