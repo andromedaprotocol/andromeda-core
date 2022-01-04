3262ec644ba78ac1a23fc91c087b06e67eceb186
Connor Barr <crnbarr@gmail.com>
2022-01-04 13:31:01 +0000

    formatting

--- contracts/andromeda_anchor/src/contract.rs
+++ contracts/andromeda_anchor/src/contract.rs
@@ -1,7 +1,15 @@
-use cosmwasm_std::{attr, entry_point, to_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdError, StdResult, Uint128, WasmMsg, CosmosMsg, coin, SubMsg};
-use cw20::{Cw20ExecuteMsg};
-use crate::state::{Config, CONFIG, KEY_POSITION_IDX, POSITION, Position, PREV_AUST_BALANCE };
-use andromeda_protocol::{error::ContractError, ownership::{execute_update_owner, query_contract_owner, CONTRACT_OWNER}, anchor::{ExecuteMsg, InstantiateMsg, QueryMsg}, require};
+use crate::state::{Config, Position, CONFIG, KEY_POSITION_IDX, POSITION, PREV_AUST_BALANCE};
+use andromeda_protocol::{
+    anchor::{ExecuteMsg, InstantiateMsg, QueryMsg},
+    error::ContractError,
+    ownership::{execute_update_owner, query_contract_owner, CONTRACT_OWNER},
+    require,
+};
+use cosmwasm_std::{
+    attr, coin, entry_point, to_binary, BankMsg, Binary, CosmosMsg, Deps, DepsMut, Env,
+    MessageInfo, Reply, Response, StdError, StdResult, SubMsg, Uint128, WasmMsg,
+};
+use cw20::Cw20ExecuteMsg;
 use terraswap::querier::{query_balance, query_token_balance};
 
 use andromeda_protocol::anchor::{AnchorMarketMsg, ConfigResponse, YourselfMsg};
@@ -13,7 +21,7 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    let config =Config{
+    let config = Config {
         anchor_mint: deps.api.addr_canonicalize(&msg.anchor_mint)?,
         anchor_token: deps.api.addr_canonicalize(&msg.anchor_token)?,
         stable_denom: msg.stable_denom,
@@ -23,11 +31,7 @@ pub fn instantiate(
     PREV_AUST_BALANCE.save(deps.storage, &Uint128::zero())?;
 
     CONTRACT_OWNER.save(deps.storage, &info.sender.clone())?;
-    Ok(Response::new()
-        .add_attributes(vec![
-            attr("action", "instantiate"),
-            attr("type", "anchor"),
-        ]))
+    Ok(Response::new().add_attributes(vec![attr("action", "instantiate"), attr("type", "anchor")]))
 }
 
 #[entry_point]
@@ -35,33 +39,35 @@ pub fn execute(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
-    msg: ExecuteMsg
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-
     match msg {
         ExecuteMsg::Deposit {} => execute_deposit(deps, env, info),
-        ExecuteMsg::Withdraw{ position_idx } => withdraw(deps, env, info, position_idx ),
+        ExecuteMsg::Withdraw { position_idx } => withdraw(deps, env, info, position_idx),
         ExecuteMsg::Yourself { yourself_msg } => {
             require(
                 info.sender == env.contract.address,
                 ContractError::Unauthorized {},
             )?;
             match yourself_msg {
-                YourselfMsg::TransferUst { receiver  } => transfer_ust(deps, env, receiver),
+                YourselfMsg::TransferUst { receiver } => transfer_ust(deps, env, receiver),
             }
         }
         ExecuteMsg::UpdateOwner { address } => execute_update_owner(deps, info, address),
-
     }
 }
-pub fn execute_deposit( deps: DepsMut, env: Env,  info: MessageInfo ) -> Result<Response, ContractError> {
+pub fn execute_deposit(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+) -> Result<Response, ContractError> {
     let config = CONFIG.load(deps.storage)?;
     let coin_denom = config.stable_denom.clone();
     let depositor = info.sender.clone();
 
     require(
         info.funds.len() <= 1usize,
-        ContractError::MoreThanOneCoin {}
+        ContractError::MoreThanOneCoin {},
     )?;
 
     let payment = info
@@ -73,33 +79,38 @@ pub fn execute_deposit( deps: DepsMut, env: Env,  info: MessageInfo ) -> Result<
         })?;
     //create position
     let position_idx = KEY_POSITION_IDX.load(deps.storage)?;
-    let aust_balance = query_token_balance(&deps.querier, deps.api.addr_humanize(&config.anchor_token)?, env.contract.address)?;
+    let aust_balance = query_token_balance(
+        &deps.querier,
+        deps.api.addr_humanize(&config.anchor_token)?,
+        env.contract.address,
+    )?;
     PREV_AUST_BALANCE.save(deps.storage, &aust_balance)?;
     let payment_amount = payment.amount;
 
     POSITION.save(
         deps.storage,
         &position_idx.u128().to_be_bytes(),
-        &Position{
+        &Position {
             idx: Default::default(),
             owner: deps.api.addr_canonicalize(depositor.as_str())?,
             deposit_amount: payment_amount.clone(),
             aust_amount: Uint128::zero(),
-    })?;
+        },
+    )?;
 
     //deposit Anchor Mint
-    Ok(Response::new().add_submessage(
-        SubMsg::reply_on_success(
-        CosmosMsg::Wasm(
-            WasmMsg::Execute{
+    Ok(Response::new()
+        .add_submessage(SubMsg::reply_on_success(
+            CosmosMsg::Wasm(WasmMsg::Execute {
                 contract_addr: deps.api.addr_humanize(&config.anchor_mint)?.to_string(),
-                msg: to_binary( &AnchorMarketMsg::DepositStable{})?,
-                funds: vec![payment.clone()]
+                msg: to_binary(&AnchorMarketMsg::DepositStable {})?,
+                funds: vec![payment.clone()],
             }),
             1u64,
-        )).add_attributes(vec![
+        ))
+        .add_attributes(vec![
             attr("action", "deposit"),
-            attr("deposit_amount", payment_amount.clone())
+            attr("deposit_amount", payment_amount.clone()),
         ]))
 }
 
@@ -107,7 +118,7 @@ pub fn withdraw(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
-    position_idx: Uint128
+    position_idx: Uint128,
 ) -> Result<Response, ContractError> {
     let config = CONFIG.load(deps.storage)?;
     let position = POSITION.load(deps.storage, &position_idx.u128().to_be_bytes())?;
@@ -120,65 +131,63 @@ pub fn withdraw(
 
     POSITION.remove(deps.storage, &position_idx.u128().to_be_bytes());
 
-    Ok(Response::new().add_messages(vec![
-        CosmosMsg::Wasm(WasmMsg::Execute {
-            contract_addr: deps.api.addr_humanize(&config.anchor_token)?.to_string(),
-            msg: to_binary(&Cw20ExecuteMsg::Send {
-                contract: deps.api.addr_humanize(&config.anchor_mint)?.to_string(),
-                amount: position.aust_amount.clone(),
-                msg: to_binary(&AnchorMarketMsg::RedeemStable {})?
-            })?,
-        funds: vec![]
-        }),
-        //send UST
-        CosmosMsg::Wasm( WasmMsg::Execute {
-            contract_addr: env.contract.address.to_string(),
-            msg: to_binary( &ExecuteMsg::Yourself{
-                yourself_msg: YourselfMsg::TransferUst { receiver: deps.api.addr_humanize(&position.owner)?.to_string() }
-            })?,
-            funds: vec![]
-        })
-    ]
-    ).add_attributes(vec![
-        attr("action", "withdraw"),
-        attr("position_idx", position_idx.to_string())
-    ]))
+    Ok(Response::new()
+        .add_messages(vec![
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: deps.api.addr_humanize(&config.anchor_token)?.to_string(),
+                msg: to_binary(&Cw20ExecuteMsg::Send {
+                    contract: deps.api.addr_humanize(&config.anchor_mint)?.to_string(),
+                    amount: position.aust_amount.clone(),
+                    msg: to_binary(&AnchorMarketMsg::RedeemStable {})?,
+                })?,
+                funds: vec![],
+            }),
+            //send UST
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: env.contract.address.to_string(),
+                msg: to_binary(&ExecuteMsg::Yourself {
+                    yourself_msg: YourselfMsg::TransferUst {
+                        receiver: deps.api.addr_humanize(&position.owner)?.to_string(),
+                    },
+                })?,
+                funds: vec![],
+            }),
+        ])
+        .add_attributes(vec![
+            attr("action", "withdraw"),
+            attr("position_idx", position_idx.to_string()),
+        ]))
 }
 
-pub fn transfer_ust(
-    deps: DepsMut,
-    env: Env,
-    receiver: String
-) -> Result<Response, ContractError> {
+pub fn transfer_ust(deps: DepsMut, env: Env, receiver: String) -> Result<Response, ContractError> {
     let config = CONFIG.load(deps.storage)?;
     let transfer_amount = query_balance(
         &deps.querier,
         env.contract.address,
         config.stable_denom.clone(),
     )?;
-    Ok(
-        Response::new().add_message(
-            CosmosMsg::Bank(BankMsg::Send {
-                to_address: receiver.to_string(),
-                amount: vec![coin(transfer_amount.u128(),config.stable_denom.clone())]
-            }))
-            .add_attributes(vec![
-                attr("action", "withdraw"),
-                attr("receiver", receiver.to_string()),
-                attr("amount", transfer_amount.to_string()),
-            ])
-    )
+    Ok(Response::new()
+        .add_message(CosmosMsg::Bank(BankMsg::Send {
+            to_address: receiver.to_string(),
+            amount: vec![coin(transfer_amount.u128(), config.stable_denom.clone())],
+        }))
+        .add_attributes(vec![
+            attr("action", "withdraw"),
+            attr("receiver", receiver.to_string()),
+            attr("amount", transfer_amount.to_string()),
+        ]))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> StdResult<Response> {
     match msg.id {
-        1u64 =>{ // stores aUST amount to position
+        1u64 => {
+            // stores aUST amount to position
             let config = CONFIG.load(deps.storage)?;
             let aust_balance = query_token_balance(
                 &deps.querier,
                 deps.api.addr_humanize(&config.anchor_token)?,
-                env.contract.address
+                env.contract.address,
             )?;
 
             let prev_aust_balance = PREV_AUST_BALANCE.load(deps.storage)?;
@@ -194,12 +203,10 @@ pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> StdResult<Response> {
             Ok(Response::new().add_attributes(vec![
                 attr("action", "reply"),
                 attr("position_idx", position_idx.clone().to_string()),
-                attr("aust_amount", new_aust_balance.to_string())
+                attr("aust_amount", new_aust_balance.to_string()),
             ]))
-        },
-        _ => {
-            Err(StdError::generic_err("invalid reply id"))
         }
+        _ => Err(StdError::generic_err("invalid reply id")),
     }
 }
 
@@ -220,4 +227,3 @@ fn query_config(deps: Deps) -> StdResult<ConfigResponse> {
         stable_denom: config.stable_denom,
     })
 }
-
