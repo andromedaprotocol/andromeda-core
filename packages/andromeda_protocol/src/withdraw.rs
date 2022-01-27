45eee6c489f61311463c955695e472da545ae41b
AndrewPochapsky <andrew.pochapsky@gmail.com>
2022-01-27 11:26:53 -0500

    Add unit tests for withdrawing

--- packages/andromeda_protocol/src/withdraw.rs
+++ packages/andromeda_protocol/src/withdraw.rs
@@ -1,11 +1,6 @@
-use cosmwasm_std::{
-    attr, coin, Addr, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult, Storage, SubMsg,
-    Uint128,
-};
+use cosmwasm_std::{coin, Deps, Env, MessageInfo, Order, Response, Storage, SubMsg};
 use cw20::Cw20Coin;
 use cw_storage_plus::Map;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
 use crate::{
     communication::Recipient, error::ContractError, operators::is_operator,
@@ -69,8 +64,8 @@ pub fn execute_withdraw(
             AssetInfo::Token { contract_addr } => {
                 let balance = query_token_balance(
                     &deps.querier,
-                    env.contract.address.clone(),
                     deps.api.addr_validate(&contract_addr)?,
+                    env.contract.address.clone(),
                 )?;
                 if balance.is_zero() {
                     None
@@ -96,7 +91,129 @@ pub fn execute_withdraw(
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::testing::{mock_dependencies, mock_info};
-
     use super::*;
+    use crate::{
+        operators::OPERATORS,
+        ownership::CONTRACT_OWNER,
+        testing::mock_querier::{mock_dependencies_custom, MOCK_CW20_CONTRACT},
+    };
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{to_binary, Addr, BankMsg, CosmosMsg, SubMsg, WasmMsg};
+    use cw20::Cw20ExecuteMsg;
+
+    #[test]
+    fn test_execute_withdraw_not_authorized() {
+        let mut deps = mock_dependencies(&[]);
+        let owner = "owner";
+        CONTRACT_OWNER
+            .save(deps.as_mut().storage, &Addr::unchecked(owner))
+            .unwrap();
+        let info = mock_info("not_owner", &[]);
+        let res = execute_withdraw(
+            deps.as_ref(),
+            mock_env(),
+            info,
+            Recipient::Addr("address".to_string()),
+        );
+        assert_eq!(ContractError::Unauthorized {}, res.unwrap_err());
+    }
+
+    #[test]
+    fn test_execute_withdraw_no_funds() {
+        let mut deps = mock_dependencies(&[]);
+        let owner = "owner";
+        CONTRACT_OWNER
+            .save(deps.as_mut().storage, &Addr::unchecked(owner))
+            .unwrap();
+        let info = mock_info(owner, &[]);
+        let res = execute_withdraw(
+            deps.as_ref(),
+            mock_env(),
+            info,
+            Recipient::Addr("address".to_string()),
+        );
+        assert_eq!(ContractError::EmptyFunds {}, res.unwrap_err());
+    }
+
+    #[test]
+    fn test_execute_withdraw_native() {
+        let mut deps = mock_dependencies(&[coin(100, "uusd")]);
+        let owner = "owner";
+        CONTRACT_OWNER
+            .save(deps.as_mut().storage, &Addr::unchecked(owner))
+            .unwrap();
+        let info = mock_info(owner, &[]);
+        WITHDRAWABLE_TOKENS
+            .save(
+                deps.as_mut().storage,
+                "uusd",
+                &AssetInfo::NativeToken {
+                    denom: "uusd".into(),
+                },
+            )
+            .unwrap();
+        let res = execute_withdraw(
+            deps.as_ref(),
+            mock_env(),
+            info,
+            Recipient::Addr("address".to_string()),
+        )
+        .unwrap();
+        let msg = SubMsg::new(CosmosMsg::Bank(BankMsg::Send {
+            to_address: "address".to_string(),
+            amount: vec![coin(100, "uusd")],
+        }));
+        assert_eq!(
+            Response::new()
+                .add_submessage(msg)
+                .add_attribute("action", "withdraw")
+                .add_attribute("recipient", "Addr(\"address\")"),
+            res
+        );
+    }
+
+    #[test]
+    fn test_execute_withdraw_cw20() {
+        let mut deps = mock_dependencies_custom(&[]);
+        let operator = "operator";
+        CONTRACT_OWNER
+            .save(deps.as_mut().storage, &Addr::unchecked("owner"))
+            .unwrap();
+        OPERATORS
+            .save(deps.as_mut().storage, operator, &true)
+            .unwrap();
+        let info = mock_info(operator, &[]);
+        WITHDRAWABLE_TOKENS
+            .save(
+                deps.as_mut().storage,
+                MOCK_CW20_CONTRACT,
+                &AssetInfo::Token {
+                    contract_addr: MOCK_CW20_CONTRACT.into(),
+                },
+            )
+            .unwrap();
+        let res = execute_withdraw(
+            deps.as_ref(),
+            mock_env(),
+            info,
+            Recipient::Addr("address".to_string()),
+        )
+        .unwrap();
+        let msg = SubMsg::new(WasmMsg::Execute {
+            contract_addr: MOCK_CW20_CONTRACT.into(),
+            msg: to_binary(&Cw20ExecuteMsg::Transfer {
+                recipient: "address".to_string(),
+                amount: 10u128.into(),
+            })
+            .unwrap(),
+            funds: vec![],
+        });
+        assert_eq!(
+            Response::new()
+                .add_submessage(msg)
+                .add_attribute("action", "withdraw")
+                .add_attribute("recipient", "Addr(\"address\")"),
+            res
+        );
+    }
 }
