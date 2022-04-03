e3d35f4d5d34bc3e4241d6a672d06719587fd06e
AndrewPochapsky <andrew.pochapsky@gmail.com>
2022-04-03 15:08:39 -0400

    Add logic to stake cw20 tokens

--- contracts/andromeda-cw20-staking/src/contract.rs
+++ contracts/andromeda-cw20-staking/src/contract.rs
@@ -1,15 +1,19 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
+    from_binary, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, QuerierWrapper, Response,
+    StdResult, Uint128,
 };
 use cw2::{get_contract_version, set_contract_version};
+use cw20::Cw20ReceiveMsg;
 use cw_asset::AssetInfo;
 
-use crate::state::{Config, State, CONFIG, STATE};
+use crate::state::{Config, State, CONFIG, STAKERS, STATE};
 use ado_base::ADOContract;
-use andromeda_protocol::cw20_staking::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
-use common::{ado_base::InstantiateMsg as BaseInstantiateMsg, error::ContractError};
+use andromeda_protocol::cw20_staking::{
+    Cw20HookMsg, ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg,
+};
+use common::{ado_base::InstantiateMsg as BaseInstantiateMsg, error::ContractError, require};
 
 // Version info, for migration info
 const CONTRACT_NAME: &str = "crates.io:andromeda-cw20-staking";
@@ -62,11 +66,100 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    Ok(Response::new())
+    match msg {
+        ExecuteMsg::Receive(msg) => receive_cw20(deps, env, info, msg),
+        ExecuteMsg::AndrReceive(msg) => {
+            ADOContract::default().execute(deps, env, info, msg, execute)
+        }
+        ExecuteMsg::AddRewardToken { asset_info } => panic!(),
+        ExecuteMsg::WithdrawTokens { assets } => panic!(),
+    }
+}
+
+fn receive_cw20(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: Cw20ReceiveMsg,
+) -> Result<Response, ContractError> {
+    match from_binary(&msg.msg)? {
+        Cw20HookMsg::StakeTokens {} => {
+            execute_stake_tokens(deps, env, msg.sender, info.sender.to_string(), msg.amount)
+        }
+    }
+}
+
+/// The foundation for this approach is inspired by Anchor's staking implementation:
+/// https://github.com/Anchor-Protocol/anchor-token-contracts/blob/15c9d6f9753bd1948831f4e1b5d2389d3cf72c93/contracts/gov/src/staking.rs#L15
+fn execute_stake_tokens(
+    deps: DepsMut,
+    env: Env,
+    sender: String,
+    token_address: String,
+    amount: Uint128,
+) -> Result<Response, ContractError> {
+    let contract = ADOContract::default();
+    let config = CONFIG.load(deps.storage)?;
+
+    let mission_contract = contract.get_mission_contract(deps.storage)?;
+    let staking_token_address =
+        config
+            .staking_token
+            .get_address(deps.api, &deps.querier, mission_contract)?;
+    require(
+        token_address == staking_token_address,
+        ContractError::InvalidFunds {
+            msg: "Deposited cw20 token is not the staking token".to_string(),
+        },
+    )?;
+
+    let mut state = STATE.load(deps.storage)?;
+    let mut staker = STAKERS.may_load(deps.storage, &sender)?.unwrap_or_default();
+
+    let staking_token = AssetInfo::cw20(deps.api.addr_validate(&staking_token_address)?);
+    let reward_tokens = [config.additional_reward_tokens, vec![staking_token]].concat();
+
+    // Balance already increased, so subtract deposit amount
+    let total_balance = query_token_balances(
+        &deps.querier,
+        reward_tokens,
+        env.contract.address.to_string(),
+    )?
+    .checked_sub(amount)?;
+
+    let share = if total_balance.is_zero() || state.total_share.is_zero() {
+        amount
+    } else {
+        amount.multiply_ratio(state.total_share, total_balance)
+    };
+
+    staker.share += share;
+    state.total_share += share;
+
+    STATE.save(deps.storage, &state)?;
+    STAKERS.save(deps.storage, &sender, &staker)?;
+
+    Ok(Response::new()
+        .add_attribute("action", "stake_tokens")
+        .add_attribute("sender", sender)
+        .add_attribute("share", share)
+        .add_attribute("amount", amount))
+}
+
+fn query_token_balances(
+    querier: &QuerierWrapper,
+    tokens: Vec<AssetInfo>,
+    address: String,
+) -> Result<Uint128, ContractError> {
+    let mut balance = Uint128::zero();
+    for token in tokens {
+        balance = balance.checked_add(token.query_balance(querier, address.clone())?)?;
+    }
+    Ok(balance)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
