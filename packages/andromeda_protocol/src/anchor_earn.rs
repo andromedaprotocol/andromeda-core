9501b0215438285aa013f94f7fa5b398645f62cf
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2022-05-04 12:12:50 -0400

    CU-294x2x5 | Split up anchor into "earn" and "lend" parts (#158)
    * Create fresh contract 'andromeda-anchor-earn'
    
    * Anchor earn logic moved to its own contract
    
    * Remove anchor-earn functions from original contract
    
    * Fix build
    
    * Remove unneeded files
    
    * Rename original anchor contract to anchor-lend
    
    * update schema

---
+++ packages/andromeda_protocol/src/anchor_earn.rs
@@ -0,0 +1,31 @@
+use common::ado_base::{recipient::Recipient, AndromedaMsg, AndromedaQuery};
+use cosmwasm_std::Uint128;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub primitive_contract: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    AndrReceive(AndromedaMsg),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    AndrQuery(AndromedaQuery),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PositionResponse {
+    pub recipient: Recipient,
+    pub aust_amount: Uint128,
+}
