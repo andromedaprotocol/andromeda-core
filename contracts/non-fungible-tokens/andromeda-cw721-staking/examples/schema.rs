a569c8e96b5c2f8405a5d896dc2f30f7b275296d
Joe Monem <jmonem@icloud.com>
2022-07-08 15:16:41 +0300

    schema adjustment

--- contracts/non-fungible-tokens/andromeda-cw721-staking/examples/schema.rs
+++ contracts/non-fungible-tokens/andromeda-cw721-staking/examples/schema.rs
@@ -3,9 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use andromeda_non_fungible_tokens::auction::{
-    AuctionIdsResponse, AuctionStateResponse, BidsResponse, ExecuteMsg, InstantiateMsg, QueryMsg,
-};
+use andromeda_non_fungible_tokens::cw721_staking::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -16,8 +14,4 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-
-    export_schema(&schema_for!(AuctionStateResponse), &out_dir);
-    export_schema(&schema_for!(AuctionIdsResponse), &out_dir);
-    export_schema(&schema_for!(BidsResponse), &out_dir);
 }
