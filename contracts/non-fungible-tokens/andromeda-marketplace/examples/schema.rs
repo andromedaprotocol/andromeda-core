b66ba070cd2a1fb7824ffc228e41421b17432ebb
Connor Barr <crnbarr@gmail.com>
2022-10-07 14:03:59 +0100

    Added receive schema to marketplace

--- contracts/non-fungible-tokens/andromeda-marketplace/examples/schema.rs
+++ contracts/non-fungible-tokens/andromeda-marketplace/examples/schema.rs
@@ -1,11 +1,15 @@
-use andromeda_non_fungible_tokens::marketplace::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use cosmwasm_schema::write_api;
+use std::env::current_dir;
+
+use andromeda_non_fungible_tokens::marketplace::{ExecuteMsg, InstantiateMsg, QueryMsg, Cw721HookMsg};
+use cosmwasm_schema::{write_api, schema_for, export_schema_with_title};
 
 fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
     write_api! {
         instantiate: InstantiateMsg,
         query: QueryMsg,
         execute: ExecuteMsg,
-
-    }
+    };
+    export_schema_with_title(&schema_for!(Cw721HookMsg), &out_dir, "cw721receive");
 }
