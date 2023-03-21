9c8ad6609d30cfa8d24f70064d3e9ace53ef3ac5
Joe Monem <jmonem@icloud.com>
2023-03-21 16:53:36 +0200

    clippy fix

--- contracts/ecosystem/andromeda-message-bridge/src/contract.rs
+++ contracts/ecosystem/andromeda-message-bridge/src/contract.rs
@@ -1,4 +1,4 @@
-use std::any::TypeId;
+// use std::any::TypeId;
 
 use andromeda_ibc::message_bridge::{ExecuteMsg, IbcExecuteMsg, InstantiateMsg, QueryMsg};
 
@@ -7,8 +7,8 @@ use common::{ado_base::InstantiateMsg as BaseInstantiateMsg, encode_binary, erro
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    attr, ensure, from_binary, to_binary, Binary, Deps, DepsMut, Env, IbcMsg, IbcTimeout,
-    MessageInfo, Response, StdError, WasmMsg,
+    attr, ensure, to_binary, Binary, Deps, DepsMut, Env, IbcMsg, IbcTimeout, MessageInfo, Response,
+    StdError, WasmMsg,
 };
 use cw2::set_contract_version;
 // use serde::de::DeserializeOwned;
@@ -102,24 +102,11 @@ pub fn execute_save_channel(
 }
 
 /// called on IBC packet receive in other chain
-pub fn try_wasm_msg(_deps: DepsMut, target: String, message: Binary) -> Result<WasmMsg, StdError>
-// where
-//     T: DeserializeOwned,
-{
-    let _unpacked_message = from_binary(&message)?;
-    //TODO change String to AMPPkt once it's been merged with AMP
-    let wasm_msg = if TypeId::of::<i32>() == TypeId::of::<String>() {
-        WasmMsg::Execute {
-            contract_addr: "kernel_address".to_owned(),
-            msg: message,
-            funds: vec![],
-        }
-    } else {
-        WasmMsg::Execute {
-            contract_addr: target,
-            msg: message,
-            funds: vec![],
-        }
+pub fn try_wasm_msg(_deps: DepsMut, target: String, message: Binary) -> Result<WasmMsg, StdError> {
+    let wasm_msg = WasmMsg::Execute {
+        contract_addr: target,
+        msg: message,
+        funds: vec![],
     };
     Ok(wasm_msg)
 }
