56c2d7855cedd253bbce9821fd0f9c6bd3c7e9cf
Joe Monem <jmonem@icloud.com>
2023-06-01 14:18:36 +0300

    lockdrop updates

---
+++ contracts/fungible-tokens/andromeda-lockdrop/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod contract;
+pub mod state;
+#[cfg(test)]
+pub mod testing;
--- packages/andromeda-fungible-tokens/src/lib.rs
+++ packages/andromeda-fungible-tokens/src/lib.rs
@@ -2,4 +2,4 @@
 pub mod cw20;
 pub mod cw20_exchange;
 pub mod cw20_staking;
-// pub mod lockdrop;
+pub mod lockdrop;
