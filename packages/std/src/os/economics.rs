264f0e87965c2e08a0880fce609c03db968225b5
Connor Barr <crnbarr@gmail.com>
2024-04-05 13:31:34 +0100

    Fixed imports

--- packages/std/src/os/economics.rs
+++ packages/std/src/os/economics.rs
@@ -2,15 +2,7 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Addr, Uint128};
 use cw20::Cw20ReceiveMsg;
 
-use crate::{
-    ado_base::{
-        ado_type::TypeResponse,
-        kernel_address::KernelAddressResponse,
-        ownership::{ContractOwnerResponse, OwnershipMessage},
-        version::VersionResponse,
-    },
-    amp::AndrAddr,
-};
+use crate::{ado_base::ownership::OwnershipMessage, amp::AndrAddr};
 
 #[cw_serde]
 pub struct InstantiateMsg {
@@ -74,13 +66,13 @@ pub enum QueryMsg {
     #[returns(Uint128)]
     Balance { asset: String, address: AndrAddr },
     // Base queries
-    #[returns(VersionResponse)]
+    #[returns(crate::ado_base::version::VersionResponse)]
     Version {},
-    #[returns(TypeResponse)]
+    #[returns(crate::ado_base::ado_type::TypeResponse)]
     Type {},
-    #[returns(ContractOwnerResponse)]
+    #[returns(crate::ado_base::ownership::ContractOwnerResponse)]
     Owner {},
-    #[returns(KernelAddressResponse)]
+    #[returns(crate::ado_base::kernel_address::KernelAddressResponse)]
     KernelAddress {},
 }
 
