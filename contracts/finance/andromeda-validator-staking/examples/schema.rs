57ad2336f41e2920e8d620f2400af635c3bdd8a1
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-06-28 18:24:15 +0530

    fix: Schema generation for validator staking

--- contracts/finance/andromeda-validator-staking/examples/schema.rs
+++ contracts/finance/andromeda-validator-staking/examples/schema.rs
@@ -1,8 +1,10 @@
-use andromeda_finance::validator_staking::InstantiateMsg;
+use andromeda_finance::validator_staking::{InstantiateMsg, ExecuteMsg, QueryMsg};
 use cosmwasm_schema::write_api;
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
+        execute:ExecuteMsg,
+        query:QueryMsg
     }
 }
