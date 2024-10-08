286872f848659e281e1874484551d4ef2fc44992
Joe Monem <jmonem@icloud.com>
2024-10-08 16:40:16 +0300

    test: add ics20-orch integration

---
+++ contracts/fungible-tokens/andromeda-ics20/examples/schema.rs
@@ -0,0 +1,10 @@
+use andromeda_fungible_tokens::cw20::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg
+    }
+}
