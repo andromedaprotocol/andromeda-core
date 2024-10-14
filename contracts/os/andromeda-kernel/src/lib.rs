2878f78ca6c5fe86165219adcc37e4ea939b0fb4
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-10-15 00:13:29 +0530

    fix: lint

--- contracts/os/andromeda-kernel/src/lib.rs
+++ contracts/os/andromeda-kernel/src/lib.rs
@@ -4,12 +4,11 @@ mod execute;
 pub mod ibc;
 #[cfg(all(not(target_arch = "wasm32"), feature = "testing"))]
 pub mod mock;
+mod proto;
 mod query;
 pub mod reply;
 mod state;
 mod sudo;
-mod proto;
-
 
 #[cfg(test)]
 mod testing;
