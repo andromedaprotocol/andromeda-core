a4d61bc6125444b6b68c8ae612fd35e40ecfbe3a
mdjakovic0920 <mdjakovic0920@gmail.com>
2024-10-16 06:55:28 +0000

    fix: fixed handling potential JSON parsing errors when creating data_instance

--- contracts/modules/andromeda-schema/src/query.rs
+++ contracts/modules/andromeda-schema/src/query.rs
@@ -1,9 +1,8 @@
 use andromeda_modules::schema::ValidateDataResponse;
 use andromeda_std::error::ContractError;
 use cosmwasm_std::Storage;
-use cw_json::JSON;
 use jsonschema_valid::{schemas, Config};
-use serde_json::json;
+use serde_json::{from_str, json};
 
 use crate::state::SCHEMA;
 
@@ -12,7 +11,9 @@ pub fn validate_data(
     data: String,
 ) -> Result<ValidateDataResponse, ContractError> {
     let schema = json!(SCHEMA.load(storage)?);
-    let data_instance = json!(JSON::from(data.as_str()));
+    let data_instance = from_str(data.as_str()).map_err(|_| ContractError::CustomError {
+        msg: "Invalid data JSON".to_string(),
+    })?;
 
     let config_from_schema = Config::from_schema(&schema, Some(schemas::Draft::Draft7));
 
