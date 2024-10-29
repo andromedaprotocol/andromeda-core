1c04eeb20ff162da19780966791fbc2caa59b329
mdjakovic0920 <mdjakovic0920@gmail.com>
2024-10-29 06:31:31 +0000

    feat: added mock to form ADO

--- contracts/data-storage/andromeda-form/src/lib.rs
+++ contracts/data-storage/andromeda-form/src/lib.rs
@@ -5,5 +5,5 @@ pub mod state;
 #[cfg(test)]
 pub mod testing;
 
-// #[cfg(all(not(target_arch = "wasm32"), feature = "testing"))]
-// pub mod mock;
+#[cfg(all(not(target_arch = "wasm32"), feature = "testing"))]
+pub mod mock;
