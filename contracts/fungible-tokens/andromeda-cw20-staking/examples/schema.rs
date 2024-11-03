66c7fe37f0da0b8b73107388a39dc5af272ea3d3
Anshudhar Kumar Singh <53444123+SlayerAnsh@users.noreply.github.com>
2024-11-04 02:02:35 +0530

    fix: add cw20 missing receive schema (#594)
    (cherry picked from commit cc51e269c2c3ae17fbfd887ae11e3f55c4562d77)
    
    # Conflicts:
    #	CHANGELOG.md

--- contracts/fungible-tokens/andromeda-cw20-staking/examples/schema.rs
+++ contracts/fungible-tokens/andromeda-cw20-staking/examples/schema.rs
@@ -1,11 +1,16 @@
-use andromeda_fungible_tokens::cw20_staking::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use cosmwasm_schema::write_api;
+use std::env::current_dir;
+
+use andromeda_fungible_tokens::cw20_staking::{Cw20HookMsg, ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::{export_schema_with_title, schema_for, write_api};
 
 fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
     write_api! {
         instantiate: InstantiateMsg,
         query: QueryMsg,
         execute: ExecuteMsg,
 
     }
+    export_schema_with_title(&schema_for!(Cw20HookMsg), &out_dir, "cw20receive");
 }
--- contracts/os/andromeda-economics/examples/schema.rs
+++ contracts/os/andromeda-economics/examples/schema.rs
@@ -1,11 +1,15 @@
-use andromeda_std::os::economics::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use cosmwasm_schema::write_api;
+use std::env::current_dir;
+
+use andromeda_std::os::economics::{Cw20HookMsg, ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::{export_schema_with_title, schema_for, write_api};
 
 fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
     write_api! {
         instantiate: InstantiateMsg,
         query: QueryMsg,
         execute: ExecuteMsg,
-
     }
+    export_schema_with_title(&schema_for!(Cw20HookMsg), &out_dir, "cw20receive");
 }
