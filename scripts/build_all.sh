b4b978c9e3cd950edcb8cfc7a0071198945ad7ec
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-11-05 23:00:11 +0530

    fix: append version after build only if version is not present

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -10,6 +10,10 @@ for contract in $OUT_DIR/*.wasm; do
   echo "Processing $contract"
   contract_name=`basename $contract`
   contract_name=${contract_name//.wasm/}
+  if [[ "$contract_name" == *"@"* ]]; then
+    echo "Skipping $contract_name as it already has a version."
+    continue
+  fi
   formated_name=${contract_name//_/-}
   version=$(cargo pkgid $formated_name | cut -d# -f2 | cut -d: -f2)
   mv "$contract" "$OUT_DIR/$contract_name@$version.wasm"
