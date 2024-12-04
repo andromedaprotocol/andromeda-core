fd1e8cc8ae733592fa897e1a03f7f9bffe75d418
Connor Barr <crnbarr@gmail.com>
2024-12-04 11:25:17 +0000

    feat: added mainnets

--- packages/deploy/src/chains/mod.rs
+++ packages/deploy/src/chains/mod.rs
@@ -1,12 +1,14 @@
 pub mod devnets;
+pub mod mainnets;
 pub mod testnets;
 
 use cw_orch::prelude::ChainInfo;
 use devnets::DEVNET_CHAINS;
+use mainnets::MAINNET_CHAINS;
 use testnets::TESTNET_CHAINS;
 
 pub fn get_chain(chain: String) -> ChainInfo {
-    [TESTNET_CHAINS, DEVNET_CHAINS]
+    [MAINNET_CHAINS, TESTNET_CHAINS, DEVNET_CHAINS]
         .concat()
         .iter()
         .find(|c| c.chain_id == chain || c.network_info.chain_name == chain)
