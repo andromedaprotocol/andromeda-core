e8dc9ffc2d92321b1de83e9fea24d4d445214203
Joe Monem <66594578+joemonem@users.noreply.github.com>
2024-12-11 14:31:01 +0200

    feat: add cw20 support for splitter (#703)
    Co-authored-by: Connor Barr <crnbarr@gmail.com>

--- packages/andromeda-finance/src/splitter.rs
+++ packages/andromeda-finance/src/splitter.rs
@@ -8,6 +8,7 @@ use andromeda_std::{
 };
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{ensure, BlockInfo, Decimal, Deps};
+use cw20::Cw20ReceiveMsg;
 
 #[cw_serde]
 pub struct AddressPercent {
@@ -48,17 +49,31 @@ impl InstantiateMsg {
     }
 }
 
+#[cw_serde]
+pub enum Cw20HookMsg {
+    Send { config: Option<Vec<AddressPercent>> },
+}
+
 #[andr_exec]
 #[cw_serde]
 pub enum ExecuteMsg {
     /// Update the recipients list. Only executable by the contract owner when the contract is not locked.
-    UpdateRecipients { recipients: Vec<AddressPercent> },
+    UpdateRecipients {
+        recipients: Vec<AddressPercent>,
+    },
     /// Used to lock/unlock the contract allowing the config to be updated.
-    UpdateLock { lock_time: Expiry },
+    UpdateLock {
+        lock_time: Expiry,
+    },
     /// Update the default recipient. Only executable by the contract owner when the contract is not locked.
-    UpdateDefaultRecipient { recipient: Option<Recipient> },
+    UpdateDefaultRecipient {
+        recipient: Option<Recipient>,
+    },
+    Receive(Cw20ReceiveMsg),
     /// Divides any attached funds to the message amongst the recipients list.
-    Send { config: Option<Vec<AddressPercent>> },
+    Send {
+        config: Option<Vec<AddressPercent>>,
+    },
 }
 
 #[andr_query]
