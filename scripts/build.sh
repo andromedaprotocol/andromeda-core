464928aca37da75e1625111ba04c64a06ee630d0
Connor Barr <crnbarr@gmail.com>
2024-12-22 09:06:28 +0000

    ci: build script changes for sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -67,7 +67,7 @@ build_all() {
 is_contract() {
     for directory in contracts/*/; do
         for contract in $directory/*/; do
-            if [[ "$(basename $contract)" = "$1" ]]; then
+            if [ "$(basename $contract)" = "$1" ]; then
                 return 0
             fi
         done
@@ -77,7 +77,7 @@ is_contract() {
 
 is_category() {
     for directory in contracts/*/; do
-        if [[ "$(basename $directory)" = "$1" ]]; then
+        if [ "$(basename $directory)" = "$1" ]; then
             return 0
         fi
     done
