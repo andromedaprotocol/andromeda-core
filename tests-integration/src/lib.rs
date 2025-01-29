289fa79875a3d69162f28b1ac34702e5b11f9bd4
Dimitrije Dragasevic <dimitrije.dragasevic@gmail.com>
2025-01-29 17:44:38 +0100

    Made deployment macro

--- tests-integration/src/lib.rs
+++ tests-integration/src/lib.rs
@@ -1,1 +1,57 @@
+#[macro_export]
+macro_rules! ado_deployer {
+    ($macro_name:ident, $chain_type:ty, $contract_type:ty, $msg_type:ty) => {
+        #[macro_export]
+        macro_rules! $macro_name {
+            ($contract:expr, $chain:expr, $msg:expr, $ado_name:expr) => {{
+                
+                let chain: &$chain_type = $chain;
+                let contract: $contract_type = $contract;
+                let msg: $msg_type = $msg;
+                
+                contract.upload().unwrap();
+                contract.instantiate(msg, None, None).unwrap();
+        
+                chain.aos.adodb.execute(
+                    &os::adodb::ExecuteMsg::Publish {
+                        code_id: contract.code_id().unwrap(),
+                        ado_type: $ado_name.to_string(),
+                        version: "1.0.0".to_string(),
+                        publisher: None,
+                        action_fees: None,
+                    },
+                    None,
+                ).unwrap();
 
+                contract
+            }};
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! execute_ibc_transfer {
+    ($chain2:expr, $message:expr, $denom:expr, $amount:expr) => {{
+        let tx = $chain2.aos.kernel.execute(
+            &os::kernel::ExecuteMsg::Send { message: $message },
+            Some(&[Coin {
+                denom: $denom,
+                amount: $amount,
+            }]),
+        ).unwrap();
+        tx
+    }};
+}
+
+#[macro_export]
+macro_rules! verify_balances {
+    ($chain:expr, $addresses:expr, $ibc_denom:expr) => {{
+        let balance1 = $chain.chain.query_all_balances($addresses[0].clone()).unwrap();
+        let balance2 = $chain.chain.query_all_balances($addresses[1].clone()).unwrap();
+        
+        assert_eq!(balance1[0].denom, $ibc_denom);
+        assert_eq!(balance2[0].denom, $ibc_denom);
+        assert_eq!(balance1[0].amount, Uint128::new(60));
+        assert_eq!(balance2[0].amount, Uint128::new(40));
+    }};
+}
--- tests-integration/tests/tests-ibc/splitter_ibc.rs
+++ tests-integration/tests/tests-ibc/splitter_ibc.rs
@@ -4,6 +4,7 @@ use std::vec;
 
 use andromeda_splitter::SplitterContract;
 use cosmwasm_std::{to_json_binary, Coin, Decimal, Uint128};
+use cw_orch::mock::MockBase;
 use andromeda_std::{
     amp::{messages::AMPMsg, recipient::{self, Recipient}, AndrAddr},
     os
@@ -16,16 +17,20 @@ use andromeda_testing::{
 use cw_orch::prelude::*;
 use cw_orch_interchain::prelude::*;
 use andromeda_finance::splitter::{InstantiateMsg, AddressPercent};
-
-
+use tests_integration::ado_deployer;
 
 pub struct ChainMap<'a> {
     pub chains: Vec<(&'a InterchainChain, &'a InterchainChain)>,
 }
 
+ado_deployer!(
+    deploy_splitter,
+    InterchainChain,
+    SplitterContract<MockBase>,
+    &InstantiateMsg
+);
 #[test]
 fn run_splitter_test_on_multiple_combos() {
-
     let InterchainTestEnv {
         juno,
         osmosis,
@@ -33,7 +38,6 @@ fn run_splitter_test_on_multiple_combos() {
         interchain,
         ..
     } = InterchainTestEnv::new();
-
 
     let chain_combos = ChainMap {
         chains: vec![
@@ -43,145 +47,130 @@ fn run_splitter_test_on_multiple_combos() {
         ],
     };
 
-    for(chain1, chain2) in &chain_combos.chains {
-        let splitter = SplitterContract::new(chain1.chain.clone());
-        splitter.upload().unwrap();
-    println!("Running test for chain1: {} and chain2: {}", chain1.chain_name, chain2.chain_name);
-        // Initialize with percentage splits
-    splitter
-    .instantiate(
-        &InstantiateMsg {
-            recipients: vec![
-                AddressPercent {
-                    recipient: Recipient{
-                        address: AndrAddr::from_string(chain1.addresses[0].clone()),
-                        msg: None,
-                        ibc_recovery_address: None,
-                    },
-                    percent: Decimal::percent(60),
-                },
-                AddressPercent {
-                    recipient: Recipient{
-                        address: AndrAddr::from_string(chain1.addresses[1].clone()),
-                        msg: None,
-                        ibc_recovery_address: None,
-                    },
-                    percent: Decimal::percent(40),
-                },
-            ],
-            kernel_address: chain1.aos.kernel.address().unwrap().into_string(),
-            owner: None,
-            lock_time: None,
-            default_recipient: None,
-        },
-        None,
-        None,
-    )
-    .unwrap();
+    for (chain1, chain2) in &chain_combos.chains {
+        println!("Running test for chain1: {} and chain2: {} combo", 
+                chain1.chain_name, chain2.chain_name);
 
-    chain1
-        .aos
-        .adodb
-        .execute(
-            &os::adodb::ExecuteMsg::Publish {
-                code_id: splitter.code_id().unwrap(),
-                ado_type: "splitter".to_string(),
-                version: "1.0.0".to_string(),
-                publisher: None,
-                action_fees: None,
+        let contract = SplitterContract::new(chain1.chain.clone());
+        
+        let deployed_contract = deploy_splitter!(
+            contract,
+            chain1,
+            &InstantiateMsg {
+                recipients: vec![
+                    AddressPercent {
+                        recipient: Recipient {
+                            address: AndrAddr::from_string(chain1.addresses[0].clone()),
+                            msg: None,
+                            ibc_recovery_address: None,
+                        },
+                        percent: Decimal::percent(60),
+                    },
+                    AddressPercent {
+                        recipient: Recipient {
+                            address: AndrAddr::from_string(chain1.addresses[1].clone()),
+                            msg: None,
+                            ibc_recovery_address: None,
+                        },
+                        percent: Decimal::percent(40),
+                    },
+                ],
+                kernel_address: chain1.aos.kernel.address().unwrap().into_string(),
+                owner: None,
+                lock_time: None,
+                default_recipient: None,
             },
-            None,
-        )
-        .unwrap();
-   
+            "splitter"
+        );
 
-    // Create IBC message
-    let chain1_recipient = AndrAddr::from_string(format!(
-        "ibc://{}/{}",
-        chain1.chain_name,
-        splitter.address().unwrap()
-    ));
-    
-    let message = AMPMsg::new(
-        chain1_recipient,
-        to_json_binary(&andromeda_finance::splitter::ExecuteMsg::Send { config: None }).unwrap(),
-        Some(vec![Coin {
-            denom: chain2.denom.clone(),
-            amount: Uint128::new(100),
-        }]),
-    );
-
-
-    // Send funds from chain2
-    let chain2_send_request = chain2
-        .aos
-        .kernel
-        .execute(
-            &os::kernel::ExecuteMsg::Send { message },
-            Some(&[Coin {
+        // Now use deployed_contract for the address
+        let chain1_recipient = AndrAddr::from_string(format!(
+            "ibc://{}/{}",
+            chain1.chain_name,
+            deployed_contract.address().unwrap()
+        ));
+        
+        let message = AMPMsg::new(
+            chain1_recipient,
+            to_json_binary(&andromeda_finance::splitter::ExecuteMsg::Send { config: None }).unwrap(),
+            Some(vec![Coin {
                 denom: chain2.denom.clone(),
                 amount: Uint128::new(100),
             }]),
-        )
-        .unwrap();
+        );
 
-    let packet_lifetime = interchain
-        .await_packets(&chain2.chain_name, chain2_send_request)
-        .unwrap();
-    ensure_packet_success(packet_lifetime);
 
-   
-    let ibc_denom = format!(
-        "ibc/{}/{}",
-        chain1.aos.get_aos_channel(&chain2.chain_name).unwrap().direct.unwrap(),
-        chain2.chain_name.clone()
-    );
+        // Send funds from chain2
+        let chain2_send_request = chain2
+            .aos
+            .kernel
+            .execute(
+                &os::kernel::ExecuteMsg::Send { message },
+                Some(&[Coin {
+                    denom: chain2.denom.clone(),
+                    amount: Uint128::new(100),
+                }]),
+            )
+            .unwrap();
 
-    // Setup trigger 
-    chain2.aos
-        .kernel
-        .execute(
-            &os::kernel::ExecuteMsg::UpsertKeyAddress {
-                key: "trigger_key".to_string(),
-                value: DEFAULT_SENDER.to_string(),
-            },
-            None,
-        )
-        .unwrap();
+        let packet_lifetime = interchain
+            .await_packets(&chain2.chain_name, chain2_send_request)
+            .unwrap();
+        ensure_packet_success(packet_lifetime);
 
-    let packet_ack = make_ack_success();
-    let channel_id = chain2.aos.get_aos_channel(chain1.chain_name.clone()).unwrap().ics20.unwrap();
-    
-    // Trigger split execution
-    let kernel_chain2_splitter = chain2
-        .aos
-        .kernel
-        .execute(
-            &os::kernel::ExecuteMsg::TriggerRelay {
-                packet_sequence: 1,
-                packet_ack,
-                channel_id,
-            },
-            None,
-        )
-        .unwrap();
 
-    let packet_lifetime = interchain
-        .await_packets(&chain2.chain_name, kernel_chain2_splitter)
-        .unwrap();
-    ensure_packet_success(packet_lifetime);
+        let ibc_denom = format!(
+            "ibc/{}/{}",
+            chain1.aos.get_aos_channel(&chain2.chain_name).unwrap().direct.unwrap(),
+            chain2.chain_name.clone()
+        );
 
-    // Verify split amounts
-    let balance1 = chain1.chain.query_all_balances(chain1.addresses[0].clone()).unwrap();
-    let balance2 = chain1.chain.query_all_balances(chain1.addresses[1].clone()).unwrap();
-    
-    assert_eq!(balance1[0].denom, ibc_denom);
-    assert_eq!(balance2[0].denom, ibc_denom);
-    assert_eq!(balance1[0].amount, Uint128::new(60)); // 60%
-    assert_eq!(balance2[0].amount, Uint128::new(40)); // 40%
+        // Setup trigger 
+        chain2.aos
+            .kernel
+            .execute(
+                &os::kernel::ExecuteMsg::UpsertKeyAddress {
+                    key: "trigger_key".to_string(),
+                    value: DEFAULT_SENDER.to_string(),
+                },
+                None,
+            )
+            .unwrap();
 
+        let packet_ack = make_ack_success();
+        let channel_id = chain2.aos.get_aos_channel(chain1.chain_name.clone()).unwrap().ics20.unwrap();
+        
+        // Trigger split execution
+        let kernel_chain2_splitter = chain2
+            .aos
+            .kernel
+            .execute(
+                &os::kernel::ExecuteMsg::TriggerRelay {
+                    packet_sequence: 1,
+                    packet_ack,
+                    channel_id,
+                },
+                None,
+            )
+            .unwrap();
+
+        let packet_lifetime = interchain
+            .await_packets(&chain2.chain_name, kernel_chain2_splitter)
+            .unwrap();
+        ensure_packet_success(packet_lifetime);
+
+        // Verify split amounts
+        let balance1 = chain1.chain.query_all_balances(chain1.addresses[0].clone()).unwrap();
+        let balance2 = chain1.chain.query_all_balances(chain1.addresses[1].clone()).unwrap();
+        
+        assert_eq!(balance1[0].denom, ibc_denom);
+        assert_eq!(balance2[0].denom, ibc_denom);
+        assert_eq!(balance1[0].amount, Uint128::new(60)); // 60%
+        assert_eq!(balance2[0].amount, Uint128::new(40)); // 40%
+    }
 }
-}
+
+
 
 #[test]
 fn test_splitter_ibc_update_recipients() {
@@ -295,5 +284,4 @@ fn test_splitter_ibc_update_recipients() {
 
     assert_eq!(format!("{:?}", packets).contains("error"), true);
 }
-
 
