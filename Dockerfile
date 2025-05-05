87f05883cd86afdd15d5c78ab63ae9d9ffd67091
cowboy0015 <web3pirate@outlook.com>
2025-05-05 17:51:15 -0400

    update: updated build script to select optimizer

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
-FROM cosmwasm/optimizer:0.16.1
+ARG OPTIMIZER_IMAGE=cosmwasm/optimizer:0.16.1
+FROM ${OPTIMIZER_IMAGE}
 RUN apk add --no-cache clang
--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 mkdir -p ./artifacts
 
-docker build -t cosmwasm-optimizer-clang .
+if [[ $(uname -m) == "arm64" ]]; then
+  OPTIMIZER_IMAGE="cosmwasm/optimizer-arm64:0.16.1"
+else
+  OPTIMIZER_IMAGE="cosmwasm/optimizer:0.16.1"
+fi
 
+docker build --build-arg OPTIMIZER_IMAGE="$OPTIMIZER_IMAGE" -t cosmwasm-optimizer-clang .
+
+echo "Building contracts for $(uname -m) architecture"
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
